9a2b9b62c6f74e57bd23680a8c30747cb9361ed8
Dave Townsend <dtownsend@oxymoronical.com>
2016-03-12 20:03:40 -0800

    More boilerplate

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ BIN=node_modules/.bin
 SRC=website/src
 STATIC=website/static/website/build
 
+eslint ${SRC}/app.js
 ${BIN}/browserify ${SRC}/app.js --debug -t babelify | ${BIN}/exorcist ${STATIC}/app.js.map >${STATIC}/app.js
 ./manage.py collectstatic --no-input
