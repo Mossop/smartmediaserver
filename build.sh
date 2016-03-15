82b067610ed7d03e0163495faf25d9eef00ac84b
Dave Townsend <dtownsend@oxymoronical.com>
2016-03-15 10:38:29 -0700

    Force using the virtualenv's python when building.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+PYTHON=bin/python
 BIN=node_modules/.bin
 SRC=website/src
 STATIC=website/static/website/build
@@ -7,6 +8,6 @@ STATIC=website/static/website/build
 ${BIN}/eslint ${SRC}/*.js
 mkdir -p website/static/website/build
 ${BIN}/browserify ${SRC}/app.js --debug -t babelify | ${BIN}/exorcist ${STATIC}/app.js.map >${STATIC}/app.js
-./manage.py collectstatic --no-input
-./manage.py makemigrations website
-./manage.py migrate
+${PYTHON} ./manage.py collectstatic --no-input
+${PYTHON} ./manage.py makemigrations website
+${PYTHON} ./manage.py migrate
