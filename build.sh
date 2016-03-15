b45e0153773f8db825adcc5fd1c815d69cddcf22
Dave Townsend <dtownsend@oxymoronical.com>
2016-03-15 10:35:08 -0700

    Build database in build script.

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ ${BIN}/eslint ${SRC}/*.js
 mkdir -p website/static/website/build
 ${BIN}/browserify ${SRC}/app.js --debug -t babelify | ${BIN}/exorcist ${STATIC}/app.js.map >${STATIC}/app.js
 ./manage.py collectstatic --no-input
+./manage.py makemigrations website
+./manage.py migrate
